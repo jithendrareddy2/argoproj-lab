argoproj-labs/argo-rollouts-manager

feat: Support additional metadata for controller
Signed-off-by: Guillaume Doussin <guillaume.doussin@gmail.com>

controllers/deployment.go
@@ -26,20 +26,29 @@ func generateDesiredRolloutsDeployment(cr rolloutsmanagerv1alpha1.RolloutManager
 			Namespace: cr.Namespace,
 		},
 	}
+	setRolloutsLabelsAndAnnotations(&desiredDeployment.ObjectMeta, &cr)
 
-	setRolloutsLabels(&desiredDeployment.ObjectMeta)
+	labels := map[string]string{
+		DefaultRolloutsSelectorKey: DefaultArgoRolloutsResourceName,
+	}
+	annotations := map[string]string{}
+	if cr.Spec.ControllerMetadata != nil {
+		for k, v := range cr.Spec.ControllerMetadata.Labels {
+			labels[k] = v
+		}
+		for k, v := range cr.Spec.ControllerMetadata.Annotations {
+			annotations[k] = v
+		}
+	}
 
 	desiredDeployment.Spec = appsv1.DeploymentSpec{
 		Selector: &metav1.LabelSelector{
-			MatchLabels: map[string]string{
-				DefaultRolloutsSelectorKey: DefaultArgoRolloutsResourceName,
-			},
+			MatchLabels: labels,
 		},
 		Template: corev1.PodTemplateSpec{
 			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{
-					DefaultRolloutsSelectorKey: DefaultArgoRolloutsResourceName,
-				},
+				Labels:      labels,
+				Annotations: annotations,
 			},
 			Spec: corev1.PodSpec{
 				NodeSelector: map[string]string{
@@ -123,6 +132,7 @@ func (r *RolloutManagerReconciler) reconcileRolloutsDeployment(ctx context.Conte
 		actualDeployment.Spec.Template.Spec.ServiceAccountName = desiredDeployment.Spec.Template.Spec.ServiceAccountName
 		actualDeployment.Labels = desiredDeployment.Labels
 		actualDeployment.Spec.Template.Labels = desiredDeployment.Spec.Template.Labels
+		actualDeployment.Spec.Template.Annotations = desiredDeployment.Spec.Template.Annotations
 		actualDeployment.Spec.Selector = desiredDeployment.Spec.Selector
 		actualDeployment.Spec.Template.Spec.NodeSelector = desiredDeployment.Spec.Template.Spec.NodeSelector
 		actualDeployment.Spec.Template.Spec.Tolerations = desiredDeployment.Spec.Template.Spec.Tolerations
@@ -153,6 +163,10 @@ func identifyDeploymentDifference(x appsv1.Deployment, y appsv1.Deployment) stri
 
 	if !reflect.DeepEqual(x.Spec.Template.Labels, y.Spec.Template.Labels) {
 		return ".Spec.Template.Labels"
+	}
+
+	if !reflect.DeepEqual(x.Spec.Template.Annotations, y.Spec.Template.Annotations) {
+		return ".Spec.Template.Annotations"
 	}
 
 	if !reflect.DeepEqual(x.Spec.Selector, y.Spec.Selector) {

controllers/deployment_test.go
@@ -223,7 +223,7 @@ func deploymentCR(name string, namespace string, label string, volumeName string
 			Namespace: namespace,
 		},
 	}
-	setRolloutsLabels(&deploymentCR.ObjectMeta)
+	setRolloutsLabelsAndAnnotations(&deploymentCR.ObjectMeta, rolloutManager)
 	deploymentCR.Spec = appsv1.DeploymentSpec{
 		Selector: &metav1.LabelSelector{
 			MatchLabels: map[string]string{

jithendrareddy2/argoproj-lab

controllers/deployment.go
@@ -26,20 +26,29 @@ func generateDesiredRolloutsDeployment(cr rolloutsmanagerv1alpha1.RolloutManager
 			Namespace: cr.Namespace,
 		},
 	}
+	setRolloutsLabelsAndAnnotationsToController(&desiredDeployment.ObjectMeta, &cr)
 
-	setRolloutsLabels(&desiredDeployment.ObjectMeta)
+	labels := map[string]string{
+		DefaultRolloutsSelectorKey: DefaultArgoRolloutsResourceName,
+	}
+	annotations := map[string]string{}
+	if cr.Spec.ControllerMetadata != nil {
+		for k, v := range cr.Spec.ControllerMetadata.Labels {
+			labels[k] = v
+		}
+		for k, v := range cr.Spec.ControllerMetadata.Annotations {
+			annotations[k] = v
+		}
+	}
 
 	desiredDeployment.Spec = appsv1.DeploymentSpec{
 		Selector: &metav1.LabelSelector{
-			MatchLabels: map[string]string{
-				DefaultRolloutsSelectorKey: DefaultArgoRolloutsResourceName,
-			},
+			MatchLabels: labels,
 		},
 		Template: corev1.PodTemplateSpec{
 			ObjectMeta: metav1.ObjectMeta{
-				Labels: map[string]string{
-					DefaultRolloutsSelectorKey: DefaultArgoRolloutsResourceName,
-				},
+				Labels:      labels,
+				Annotations: annotations,
 			},
 			Spec: corev1.PodSpec{
 				NodeSelector: map[string]string{
@@ -123,6 +132,7 @@ func (r *RolloutManagerReconciler) reconcileRolloutsDeployment(ctx context.Conte
 		actualDeployment.Spec.Template.Spec.ServiceAccountName = desiredDeployment.Spec.Template.Spec.ServiceAccountName
 		actualDeployment.Labels = desiredDeployment.Labels
 		actualDeployment.Spec.Template.Labels = desiredDeployment.Spec.Template.Labels
+		actualDeployment.Spec.Template.Annotations = desiredDeployment.Spec.Template.Annotations
 		actualDeployment.Spec.Selector = desiredDeployment.Spec.Selector
 		actualDeployment.Spec.Template.Spec.NodeSelector = desiredDeployment.Spec.Template.Spec.NodeSelector
 		actualDeployment.Spec.Template.Spec.Tolerations = desiredDeployment.Spec.Template.Spec.Tolerations
@@ -153,6 +163,10 @@ func identifyDeploymentDifference(x appsv1.Deployment, y appsv1.Deployment) stri
 
 	if !reflect.DeepEqual(x.Spec.Template.Labels, y.Spec.Template.Labels) {
 		return ".Spec.Template.Labels"
+	}
+
+	if !reflect.DeepEqual(x.Spec.Template.Annotations, y.Spec.Template.Annotations) {
+		return ".Spec.Template.Annotations"
 	}
 
 	if !reflect.DeepEqual(x.Spec.Selector, y.Spec.Selector) {

controllers/deployment_test.go
@@ -223,7 +223,7 @@ func deploymentCR(name string, namespace string, label string, volumeName string
 			Namespace: namespace,
 		},
 	}
-	setRolloutsLabels(&deploymentCR.ObjectMeta)
+	setRolloutsLabelsAndAnnotationsToController(&deploymentCR.ObjectMeta, rolloutManager)
 	deploymentCR.Spec = appsv1.DeploymentSpec{
 		Selector: &metav1.LabelSelector{
 			MatchLabels: map[string]string{